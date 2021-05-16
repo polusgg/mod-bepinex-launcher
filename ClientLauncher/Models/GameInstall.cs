29cecbccc0fdb8380d09ca170421e17a195d4d56
cybershard <ashwatdd01@gmail.com>
2021-05-15 20:17:22 -0400

    Add path properties to GameInstall

--- ClientLauncher/Models/GameInstall.cs
+++ ClientLauncher/Models/GameInstall.cs
@@ -7,5 +7,24 @@ namespace ClientLauncher.Models
     {
         public string Location { get; set; } =
             Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".polusgg");
+        
+        
+        /*
+         *  Path Properties 
+         */
+        
+        public string AmongUsExe => Path.Combine(Location, "Among Us.exe");
+        
+        // Unity's globalgamemanagers file containing Among Us version
+        public string GlobalGameManagersFile => Path.Combine(Location, "Among Us_Data", "globalgamemanagers");
+
+        // Standard BepInEx paths
+        public string BepInExFolder => Path.Combine(Location, "BepInEx");
+        public string PreloaderFolder => Path.Combine(BepInExFolder, "patchers");
+        public string PluginFolder => Path.Combine(BepInExFolder, "plugins");
+
+        // Specific paths for downloaded asset hashes
+        public string BepInExVersionFile => Path.Combine(BepInExFolder, "version.txt");
+        public string PreloaderHashFile => Path.Combine(PreloaderFolder, "polusgg-preloader.md5hash");
     }
 }
